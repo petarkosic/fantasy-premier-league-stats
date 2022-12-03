074771491b7c0accb6ba15c80df236563eb97ba4
petarkosic <avt3914@gmail.com>
2022-12-03 23:59:45 +0100

    ADD - add input search

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
 	return (
 		<>
 			<div className='app'>
+				<input type='text' placeholder='search player by player name' />
 				<div>
 					Gameweek Data{' '}
 					<select name='gameweek'>
