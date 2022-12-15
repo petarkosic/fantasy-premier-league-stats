2bb14286a62eb075da207a908230355c01d238f6
petarkosic <avt3914@gmail.com>
2022-12-15 23:57:18 +0100

    ADD - card division, top and bottom

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -6,24 +6,25 @@ type PlayerCardProps = {
 
 const PlayerCard = ({ data }: PlayerCardProps) => {
 	return (
-		<div className='card'>
-			<div className='card-top'>
-				<div className='card-top--name'>
-					<p>
-						{data.first_name} {data.second_name}
-					</p>
+		<div className='card-wrapper'>
+			<div className='card'>
+				<div className='card-top'>
+					<div className='card-top--name'>
+						<p>{data.first_name}</p>
+						<p>{data.second_name}</p>
+					</div>
+					<div className='card-top--image'>
+						<p>{data.photo}</p>
+					</div>
 				</div>
-				<div className='card-top--image'>
-					<p>{data.photo}</p>
+				<div className='card-bottom'>
+					<p>{data.assists}</p>
+					<p>{data.bonus}</p>
+					<p>{data.form}</p>
+					<p>{data.expected_goals}</p>
+					<p>{data.minutes}</p>
+					<p>{data.influence}</p>
 				</div>
-			</div>
-			<div className='card-bottom'>
-				<p>{data.assists}</p>
-				<p>{data.bonus}</p>
-				<p>{data.form}</p>
-				<p>{data.expected_goals}</p>
-				<p>{data.minutes}</p>
-				<p>{data.influence}</p>
 			</div>
 		</div>
 	);
