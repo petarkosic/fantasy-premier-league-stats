d0e40ba2e4ebc384f09812b97e200cd222045e80
petarkosic <avt3914@gmail.com>
2023-03-27 23:59:19 +0200

    ADD - server side - team image route and controller

---
+++ server/src/controllers/teamController.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+
+type Error = {
+	message: string;
+};
+
+export const getTeamImage = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const { code } = req.params;
+	console.log(code);
+
+	try {
+		const response = await axios.get(
+			`https://resources.premierleague.com/premierleague/badges/70/t${code}.png`
+		);
+		let teamImage = response.config.url;
+
+		res.status(200).json({
+			teamImage,
+		});
+	} catch (err) {
+		const error = err as Error;
+		console.error(error.message);
+	}
+};
--- server/src/routes/apiRoutes.ts
+++ server/src/routes/apiRoutes.ts
@@ -5,6 +5,7 @@ import {
 	getPlayerImage,
 	getPlayerSummary,
 } from '../controllers/apiController';
+import { getTeamImage } from '../controllers/teamController';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.get('/stats', callApi);
 router.get('/player/:code', getPlayer);
 router.get('/player-summary/:id', getPlayerSummary);
 router.get('/players/:code', getPlayerImage);
+router.get('/team/:code', getTeamImage);
 
 export default router;
