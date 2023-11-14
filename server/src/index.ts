7d2234a69ccfb15ddf0ca8e54f47a117483c3a75
petarkosic <avt3914@gmail.com>
2023-11-14 23:58:27 +0100

    FIX - remove slow down

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
-import slowDown from 'express-slow-down';
 
 import apiRoutes from './routes/apiRoutes';
 
@@ -12,12 +11,6 @@ const rateLimiter = rateLimit({
 	max: 50, // Limit each IP to 50 requests per `window` (here, per 10 seconds)
 	message:
 		'Too many requests made from this IP, please try again after 10 seconds',
-});
-
-const speedLimiter = slowDown({
-	windowMs: 10 * 1000, // 10 seconds
-	delayAfter: 1, // delay after the first one
-	delayMs: 500, // delay by half a second
 });
 
 const PORT = 5000;
@@ -34,7 +27,7 @@ app.get('/', (req: Request, res: Response) => {
 	res.send('Hello from api');
 });
 
-app.use('/api', rateLimiter, speedLimiter, apiRoutes);
+app.use('/api', rateLimiter, apiRoutes);
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
