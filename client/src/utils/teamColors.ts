6966926794e684798046e3a9c3f2fc1e1838f508
petarkosic <avt3914@gmail.com>
2024-10-04 23:35:57 +0200

    add: team names map
    Map team ids to team names.
    Add new teams colors.

--- client/src/utils/teamColors.ts
+++ client/src/utils/teamColors.ts
@@ -8,14 +8,17 @@ export let teamColors: TeamColors = {
 	4: ['rgba(45, 41, 38, 1)', 'rgba(25, 21, 18, 1)'], // 'Newcastle'
 	6: ['rgba(19, 34, 87, 1)', 'rgba(0, 14, 67, 1)'], // 'Spurs'
 	7: ['rgba(149, 191, 229, 1)', 'rgba(129,171,209, 1)'], // 'Aston Villa'
-	8: ['rgba(3, 70, 148,1)', 'rgba(0, 50, 128, 1)'], // 'Chelsea'
+	8: ['rgba(3, 70, 148, 1)', 'rgba(0, 50, 128, 1)'], // 'Chelsea'
 	11: ['rgba(39,68,136, 1)', 'rgba(19,48,116, 1)'], // 'Everton'
+	13: ['rgba(0, 83, 160, 1)', 'rgba(0, 63, 140, 1)'], // Leicester
 	14: ['rgba(200, 16, 46, 1)', 'rgba(180, 0, 26, 1)'], // 'Liverpool'
 	17: ['rgba(221, 0, 0, 1)', 'rgba(201, 0, 0, 1)'], // 'Nott'm Forest'
+	20: ['rgba(215, 25, 32, 1)', 'rgba(195, 5, 12, 1)'], // Southampton
 	21: ['rgba(122, 38, 58, 1)', 'rgba(102, 18, 38, 1)'], // 'West Ham'
 	31: ['rgba(27, 69, 143, 1)', 'rgba(7, 49, 123, 1)'], // 'Crystal Palace'
 	36: ['rgba(0, 87, 184, 1)', 'rgba(0, 67, 164, 1)'], // 'Brighton'
 	39: ['rgba(253,185,19, 1)', 'rgba(233, 165, 0, 1)'], // 'Wolves'
+	40: ['rgba(58, 100, 163, 1)', 'rgba(38, 80, 143, 1)'], // Ipswich
 	43: ['rgba(108, 171, 221, 1)', 'rgba(88, 151, 201, 1)'], // 'Man City'
 	49: ['rgba(238, 39, 55, 1)', 'rgba(218, 19, 35, 1)'], // 'Sheffield Utd'
 	54: ['rgba(0, 0, 0, 1)', 'rgba(20, 20, 20, 1)'], // 'Fulham'
---
+++ client/src/utils/teamNames.ts
@@ -0,0 +1,26 @@
+type TeamName = {
+	[key: number]: string;
+};
+
+export const teamNames: TeamName = {
+	1: 'Arsenal',
+	2: 'Aston Villa',
+	3: 'Bournemouth',
+	4: 'Brentford',
+	5: 'Brighton',
+	6: 'Chelsea',
+	7: 'Crystal Palace',
+	8: 'Everton',
+	9: 'Fulham',
+	10: 'Ipswich',
+	11: 'Leicester',
+	12: 'Liverpool',
+	13: 'Man City',
+	14: 'Man Utd',
+	15: 'Newcastle',
+	16: "Nott'm Forest",
+	17: 'Southampton',
+	18: 'Tottenham',
+	19: 'West Ham',
+	20: 'Wolves',
+};
